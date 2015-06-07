583d4a4915d7c4a94ebca6b571d6bba2d876014b
Brian Gillespie <brian.j.gillespie@gmail.com>
2015-06-07 01:05:40 +0100

    Change timestamp format to mm/dd/yyyy hh:mm:ss

--- database/types/timestamp.go
+++ database/types/timestamp.go
@@ -7,8 +7,10 @@ import (
 
 type Timestamp time.Time
 
+const format = "01/02/2006 15:04:00"
+
 func (ts Timestamp) Value() (driver.Value, error) {
-	return []byte(time.Time(ts).Format(time.ANSIC)), nil
+	return []byte(time.Time(ts).Format(format)), nil
 }
 
 func (ts *Timestamp) Scan(src interface{}) error {
@@ -23,11 +25,11 @@ func (ts *Timestamp) MarshalJSON() ([]byte, error) {
 func (ts *Timestamp) UnmarshalJSON(b []byte) error {
 	// Need to strip delimiting quote marks
 	s := string(b[1 : len(b)-1])
-	t, err := time.Parse(time.ANSIC, s)
+	t, err := time.Parse(format, s)
 	*ts = Timestamp(t)
 	return err
 }
 
 func (ts Timestamp) String() string {
-	return time.Time(ts).Format(time.ANSIC)
+	return time.Time(ts).Format(format)
 }
